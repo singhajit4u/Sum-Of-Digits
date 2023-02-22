c4e153f8e18770e1e47ae743658a2a778a699e21
NFO-2 <NFO-2@NFO-2>
2023-02-22 16:25:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sum_Of_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number for finding its sum:");
+            int number =Convert.ToInt32(Console.ReadLine());
+            int sum, remainder;
+            sum = 0;
+            while (number>0)
+            {
+                remainder = number % 10;
+                sum=sum+remainder;  
+                number=number/10;
+            }
+
+            Console.Write("The sum is:"+ sum);
+        }
+    }
+}
